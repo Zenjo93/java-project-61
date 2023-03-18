859062c9359925265797bdacef51d6a6bc09cd71
Vlad Berezhnoy <vl.brzn@gmail.com>
2023-03-18 20:09:18 +0300

    fix checkStyle errors

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -5,7 +5,7 @@ import hexlet.code.UtilsRandom;
 
 public class Progression {
     private static final String GAME_RULE = "What number is missing in the progression?";
-    static int progressionLength = 10;
+    private static final int PROGRESSION_LENGTH = 10;
     private static final int MAX_LIMIT = 5;
     private static final int MIN_LIMIT = 1;
     private static String[] questions = new String[Engine.getTurnsCount()];
@@ -23,15 +23,19 @@ public class Progression {
         return rightAnswers;
     }
 
+    public static int getProgressionLength() {
+        return PROGRESSION_LENGTH;
+    }
+
     private static void setGameData() {
-        String[] progression = new String[progressionLength];
+        String[] progression = new String[PROGRESSION_LENGTH];
 
         for (var i = 0; i < Engine.getTurnsCount(); i += 1) {
             int startNumber = UtilsRandom.getRandomNumber(MIN_LIMIT, MAX_LIMIT);
             int step = UtilsRandom.getRandomNumber(MIN_LIMIT, MAX_LIMIT);
-            int blankPosition = UtilsRandom.getRandomNumber(0, progressionLength - 1);
+            int blankPosition = UtilsRandom.getRandomNumber(0, PROGRESSION_LENGTH - 1);
 
-            for (int j = 0, num = startNumber; j < progressionLength; num += step, j += 1) {
+            for (int j = 0, num = startNumber; j < PROGRESSION_LENGTH; num += step, j += 1) {
                 progression[j] = Integer.toString(num);
             }
 
